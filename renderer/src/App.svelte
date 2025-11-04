bad1d0133fd2adafe04935f36b2b1df8648ac9f1
uitgeteld <swendewit07@gmail.com>
2025-11-04 12:36:57 +0100

    renderer

---
+++ renderer/src/App.svelte
@@ -0,0 +1,19 @@
+<main>
+    <h1>The Miner Got Help</h1>
+</main>
+
+<style lang="scss">
+    main {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+        background-color: #121212;
+        color: #ffffff;
+
+        h1 {
+            font-family: 'Arial', sans-serif;
+        }
+    }
+</style>
