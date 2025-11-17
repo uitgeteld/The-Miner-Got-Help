03ea2e1b7c15c12a048b0f2b6e9945c5df06233d
uitgeteld <swendewit07@gmail.com>
2025-11-17 14:12:47 +0100

    Added a footer

--- renderer/src/App.svelte
+++ renderer/src/App.svelte
@@ -1,6 +1,13 @@
+<script>
+    import { version } from '../../package.json';
+</script>
+
 <main>
-    <h1>The Miner Got Help</h1>
 </main>
+
+<footer>
+    <p>&copy; The Miner Got Help | {version}</p>
+</footer>
 
 <style lang="scss">
     main {
@@ -8,12 +15,22 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        height: 100vh;
+        height: 95vh;
         background-color: #121212;
         color: #ffffff;
 
         h1 {
             font-family: 'Arial', sans-serif;
         }
+    }
+
+    footer {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 5vh;
+        background-color: #1e1e1e;
+        color: #888888;
+        font-size: 0.9rem;
     }
 </style>
